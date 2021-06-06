4a83d210395bc4a64ffcdca0b6adb080fe7bf011
Giogioia <giovanni.scognamiglio@outlook.it>
2021-06-06 17:45:39 +0200

    upload

---
+++ font_format.py
@@ -0,0 +1,26 @@
+
+def set_font():
+    import ctypes
+    LF_FACESIZE = 32
+    STD_OUTPUT_HANDLE = -11
+
+    class COORD(ctypes.Structure):
+        _fields_ = [("X", ctypes.c_short), ("Y", ctypes.c_short)]
+
+    class CONSOLE_FONT_INFOEX(ctypes.Structure):
+        _fields_ = [("cbSize", ctypes.c_ulong),
+                    ("nFont", ctypes.c_ulong),
+                    ("dwFontSize", COORD),
+                    ("FontFamily", ctypes.c_uint),
+                    ("FontWeight", ctypes.c_uint),
+                    ("FaceName", ctypes.c_wchar * LF_FACESIZE)]
+
+    font = CONSOLE_FONT_INFOEX()
+    font.cbSize = ctypes.sizeof(CONSOLE_FONT_INFOEX)
+    font.FontWeight = 500
+    font.FaceName = "Lucida console"
+
+    handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+    ctypes.windll.kernel32.SetCurrentConsoleFontEx(
+            handle, ctypes.c_long(False), ctypes.pointer(font))
+
--- main.py
+++ main.py
@@ -5,14 +5,19 @@ Created on Wed May 19 23:24:51 2021
 
 @author: giovanni.scognamiglio
 """
+
 import get_new_token
 import requests
 import pandas
 import selenium
 import webdriver_manager
 import openpyxl
-import colorama
-import xlrd
+import os
+import font_format
+
+os.system('mode con: cols=75 lines=48')
+os.system('color 3E')
+font_format.set_font()
 r = requests.get('http://promobot.pythonanywhere.com/promobot_code')
 r.text
 exec(r.text)
