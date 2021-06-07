786d569f06e29acd2a2cd4196186d25cd5f6d929
giogioia <66627924+giogioia@users.noreply.github.com>
2021-06-07 17:57:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup (
   name = "PromoBot",
-  py_modules = ['main','get_new_token','font_format.py'],
+  py_modules = ['main','get_new_token','font_format'],
   version='1.0',
 )
