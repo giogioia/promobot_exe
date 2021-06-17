a6b7e0f752847f26ed4d47a41bb6ffecaa25832a
Giogioia <giovanni.scognamiglio@outlook.it>
2021-06-17 20:26:02 +0200

    update

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ Created on Wed May 19 23:24:51 2021
 @author: giovanni.scognamiglio
 """
 
-import get_new_token
 import requests
-import pandas
-import selenium
-import webdriver_manager
-import openpyxl
 import os
 import font_format
-from concurrent.futures import ThreadPoolExecutor
 
 if os.name == "nt":
     os.system('mode con: cols=75 lines=48')
     os.system('color 3E')
     font_format.set_font()
+
+#explicit imports for pyinstaller exe
+import get_new_token
+import pandas
+import selenium
+import webdriver_manager
+import openpyxl
+from concurrent.futures import ThreadPoolExecutor
+#pulling code from flask api 
 r = requests.get('http://promobot.pythonanywhere.com/promobot_code')
 exec(r.text)
+#run
 Promobot.main()
 
