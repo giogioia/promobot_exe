808283be3c5e61afa2a157fda2d9bf36837f20cf
Giogioia <giovanni.scognamiglio@outlook.it>
2021-06-18 22:05:37 +0200

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 setup (
-  name = "promobot_glovo",
-  py_modules = ['main','get_new_token','font_format'],
+  name = "promobotglovo",
+  # py_modules = ['main','get_new_token','font_format'],
+  packages = ['pack'],
   version='1.0',
 )
