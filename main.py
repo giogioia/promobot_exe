be487ee17fd76f00529fbdcdc822da1df346a184
Giogioia <giovanni.scognamiglio@outlook.it>
2021-06-20 17:33:06 +0200

    update

--- main.py
+++ main.py
@@ -9,11 +9,7 @@ Created on Wed May 19 23:24:51 2021
 import requests
 import os
 import font_format
-
-if os.name == "nt":
-    os.system('mode con: cols=75 lines=48')
-    os.system('color 3E')
-    font_format.set_font()
+from importlib.machinery import SourceFileLoader
 
 #explicit imports for pyinstaller exe
 import get_new_token
@@ -21,10 +17,26 @@ import pandas
 import selenium
 import webdriver_manager
 import openpyxl
-from concurrent.futures import ThreadPoolExecutor
-#pulling code from flask api 
-r = requests.get('http://promobot.pythonanywhere.com/promobot_code')
-exec(r.text)
-#run
-Promobot.main()
+import regex
+
+def set_background():
+    if os.name == "nt":
+        os.system('mode con: cols=75 lines=48')
+        os.system('color 3E')
+        font_format.set_font()
+
+def call_promobot():
+    global Promobot
+    r = requests.get('https://el-promobot.netlify.app/code.py')
+    with open(os.path.join(os.getcwd(),'temp_module.py'),'w+') as file:
+        file.write(r.text)
+    # from temp_module import Promobot
+    temp_module = SourceFileLoader("temp_module.py", os.path.join(os.getcwd(),'temp_module.py')).load_module()    
+    Promobot = temp_module.Promobot
+    os.remove(os.path.join(os.getcwd(),'temp_module.py'))
+
+if __name__=='__main__':
+    set_background()
+    call_promobot()
+    Promobot.driver()
 
