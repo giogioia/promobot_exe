619f5b7f49cf0192fa7b5cbf6bb8aa9ac33c123f
Giogioia <giovanni.scognamiglio@outlook.it>
2021-07-02 14:09:24 +0200

    update

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import requests
 import os
 import font_format
 from importlib.machinery import SourceFileLoader
+import sys
 
 #explicit imports for pyinstaller exe
 import get_new_token
@@ -18,6 +19,8 @@ import selenium
 import webdriver_manager
 import openpyxl
 import regex
+
+exe_version = 1.1
 
 def set_background():
     if os.name == "nt":
@@ -31,12 +34,31 @@ def call_promobot():
     with open(os.path.join(os.getcwd(),'temp_module.py'),'w+') as file:
         file.write(r.text)
     # from temp_module import Promobot
-    temp_module = SourceFileLoader("temp_module.py", os.path.join(os.getcwd(),'temp_module.py')).load_module()    
+    temp_module = SourceFileLoader("temp_module.py", os.path.join(os.getcwd(),'temp_module.py')).load_module()   
     Promobot = temp_module.Promobot
     os.remove(os.path.join(os.getcwd(),'temp_module.py'))
+
+def check_version():
+    exe_version_needed = Promobot.exe_version()
+    if exe_version != exe_version_needed:
+        conf = input('A new executor is available. Download new version? [yes/no]\t')
+        if conf in ['yes','ye','y','si']:
+            print('Estimated time: ~60 seconds.\nDownload in progress... Do not close the terminal page.')
+            try:
+                r = requests.get('https://el-promobot.netlify.app/assets/PromoBot.exe')
+                with open('PromoBot.exe', 'wb') as file:
+                    file.write(r.content)
+            except Exception:
+                print('Something went wrong.\nTry downloading new bot manually from https://el-promobot.netlify.app/')
+                k=input('\nPress Enter x2 to close')
+                sys.exit(0)
+            else: 
+                print('\nNew PromoBot.exe successfully downloaded!\nClose this window and lanch Promobot.exe again')
+                k=input('\nPress Enter x2 to close')
 
 if __name__=='__main__':
     set_background()
     call_promobot()
+    check_version()
     Promobot.driver()
 
