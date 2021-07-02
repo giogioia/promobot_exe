84360e2d6a841733bc4a86b27e1b07418129c179
Giogioia <giovanni.scognamiglio@outlook.it>
2021-07-02 15:54:17 +0200

    update

--- main.py
+++ main.py
@@ -28,37 +28,19 @@ def set_background():
         os.system('color 3E')
         font_format.set_font()
 
-def call_promobot():
-    global Promobot
+def call_PromoBot():
+    global PromoBot
     r = requests.get('https://el-promobot.netlify.app/code.py')
     with open(os.path.join(os.getcwd(),'temp_module.py'),'w+') as file:
         file.write(r.text)
     # from temp_module import Promobot
     temp_module = SourceFileLoader("temp_module.py", os.path.join(os.getcwd(),'temp_module.py')).load_module()   
-    Promobot = temp_module.Promobot
+    PromoBot = temp_module.PromoBot
     os.remove(os.path.join(os.getcwd(),'temp_module.py'))
-
-def check_version():
-    exe_version_needed = Promobot.exe_version()
-    if exe_version != exe_version_needed:
-        conf = input('A new executor is available. Download new version? [yes/no]\t')
-        if conf in ['yes','ye','y','si']:
-            print('Estimated time: ~60 seconds.\nDownload in progress... Do not close the terminal page.')
-            try:
-                r = requests.get('https://el-promobot.netlify.app/assets/PromoBot.exe')
-                with open('PromoBot.exe', 'wb') as file:
-                    file.write(r.content)
-            except Exception:
-                print('Something went wrong.\nTry downloading new bot manually from https://el-promobot.netlify.app/')
-                k=input('\nPress Enter x2 to close')
-                sys.exit(0)
-            else: 
-                print('\nNew PromoBot.exe successfully downloaded!\nClose this window and lanch Promobot.exe again')
-                k=input('\nPress Enter x2 to close')
 
 if __name__=='__main__':
     set_background()
-    call_promobot()
-    check_version()
-    Promobot.driver()
+    call_PromoBot()
+    PromoBot.exe_checker(exe_version)
+    PromoBot.driver()
 
